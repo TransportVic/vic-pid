b7c3e41796c69be3ac37bc8598947dc4ea626068
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-25 10:29:44 +1100

    Update PID class to set next service info

--- static/scripts/metro-lcd/platform.mjs
+++ static/scripts/metro-lcd/platform.mjs
@@ -3,7 +3,18 @@ import PID from '../pid.mjs'
 class MetroLCDPlatformPID extends PID {
 
   #updateNextService(service) {
-    
+    $('span.next-service-sch-time').textContent = service.schTime
+
+    if (service.estTime < 1) {
+      $('span.next-service-est-time').textContent = 'NOW'
+    } else {
+      $('span.next-service-est-time').textContent = service.estTime + ' min'
+    }
+
+    $('span.next-service-destination').textContent = service.destination
+    $('span.next-service-summary').textContent = service.summary
+
+    $('div.line-marker').className = `line-marker ${service.line}`
   }
 
   updateServices(services) {
@@ -16,7 +27,7 @@ let pid = new MetroLCDPlatformPID()
 window.pid = pid
 pid.updateServices([{
   schTime: '12:34pm',
-  estTime: '9',
+  estTime: 9,
   destination: 'East Pakenham',
   summary: 'Express, Change at Dandenong for Cranbourne',
   line: 'pakenham'
