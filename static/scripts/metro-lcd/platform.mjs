59b53189b067326c9c9e0652beb395f69fa9a0c9
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-25 10:41:02 +1100

    Added test stops into platform data

--- static/scripts/metro-lcd/platform.mjs
+++ static/scripts/metro-lcd/platform.mjs
@@ -28,7 +28,23 @@ window.pid = pid
 pid.updateServices([{
   schTime: '12:34pm',
   estTime: 9,
-  destination: 'East Pakenham',
+  destination: 'Westall',
   summary: 'Express, Change at Dandenong for Cranbourne',
-  line: 'pakenham'
+  line: 'pakenham',
+  stops: [
+    { name: 'Richmond', stops: true },
+    { name: 'South Yarra', stops: true },
+    { name: 'Hawksburn', stops: false },
+    { name: 'Toorak', stops: false },
+    { name: 'Armadale', stops: false },
+    { name: 'Malvern', stops: false },
+    { name: 'Caulfield', stops: true },
+    { name: 'Carnegie', stops: true },
+    { name: 'Murrumbeena', stops: true },
+    { name: 'Hughesdale', stops: true },
+    { name: 'Oakleigh', stops: true },
+    { name: 'Huntingdale', stops: true },
+    { name: 'Clayton', stops: true },
+    { name: 'Westall', stops: true }
+  ]
 }])
