dbd4da55a2304a3ca564972e1f7b193ddb698791
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-25 17:59:55 +1100

    fix express stop margins

--- static/scripts/metro-lcd/platform.mjs
+++ static/scripts/metro-lcd/platform.mjs
@@ -42,6 +42,70 @@ class MetroLCDPlatformPID extends PID {
 
 }
 
+let bairnsdale = [
+  { name: 'Flinders Street', stops: true },
+  { name: 'Richmond', stops: false },
+  { name: 'South Yarra', stops: false },
+  { name: 'Hawksburn', stops: false },
+  { name: 'Toorak', stops: false },
+  { name: 'Armadale', stops: false },
+  { name: 'Malvern', stops: false },
+  { name: 'Caulfield', stops: false },
+  { name: 'Carnegie', stops: false },
+  { name: 'Murrumbeena', stops: false },
+  { name: 'Hughesdale', stops: false },
+  { name: 'Oakleigh', stops: false },
+  { name: 'Huntingdale', stops: false },
+  { name: 'Clayton', stops: false },
+  { name: 'Westall', stops: false },
+  { name: 'Springvale', stops: false },
+  { name: 'Sandown Park', stops: false },
+  { name: 'Noble Park', stops: false },
+  { name: 'Yarraman', stops: false },
+  { name: 'Dandenong', stops: false },
+  { name: 'Hallam', stops: false },
+  { name: 'Narre Warren', stops: false },
+  { name: 'Berwick', stops: false },
+  { name: 'Beaconsfield', stops: false },
+  { name: 'Officer', stops: false },
+  { name: 'Cardinia Road', stops: false },
+  { name: 'Pakenham', stops: true },
+  { name: 'East Pakenham', stops: false },
+  { name: 'Nar Nar Goon', stops: false },
+  { name: 'Tynong', stops: false },
+  { name: 'Garfield', stops: true },
+  { name: 'Bunyip', stops: false },
+  { name: 'Longwarry', stops: false },
+  { name: 'Drouin', stops: true },
+  { name: 'Warragul', stops: true },
+  { name: 'Yarragon', stops: false },
+  { name: 'Trafalgar', stops: false },
+  { name: 'Moe', stops: true },
+  { name: 'Morwell', stops: true },
+  { name: 'Traralgon', stops: true },
+  { name: 'Rosedale', stops: true },
+  { name: 'Sale', stops: true },
+  { name: 'Stratford', stops: true },
+  { name: 'Bairnsdale', stops: true }
+]
+
+let westall = [
+  { name: 'Richmond', stops: true },
+  { name: 'South Yarra', stops: true },
+  { name: 'Hawksburn', stops: false },
+  { name: 'Toorak', stops: false },
+  { name: 'Armadale', stops: false },
+  { name: 'Malvern', stops: false },
+  { name: 'Caulfield', stops: true },
+  { name: 'Carnegie', stops: true },
+  { name: 'Murrumbeena', stops: true },
+  { name: 'Hughesdale', stops: true },
+  { name: 'Oakleigh', stops: true },
+  { name: 'Huntingdale', stops: true },
+  { name: 'Clayton', stops: true },
+  { name: 'Westall', stops: true }
+]
+
 let pid = new MetroLCDPlatformPID()
 window.pid = pid
 pid.updateServices([{
@@ -50,20 +114,5 @@ pid.updateServices([{
   destination: 'Westall',
   summary: 'Express, Change at Dandenong for Cranbourne',
   line: 'pakenham',
-  stops: [
-    { name: 'Richmond', stops: true },
-    { name: 'South Yarra', stops: true },
-    { name: 'Hawksburn', stops: false },
-    { name: 'Toorak', stops: false },
-    { name: 'Armadale', stops: false },
-    { name: 'Malvern', stops: false },
-    { name: 'Caulfield', stops: true },
-    { name: 'Carnegie', stops: true },
-    { name: 'Murrumbeena', stops: true },
-    { name: 'Hughesdale', stops: true },
-    { name: 'Oakleigh', stops: true },
-    { name: 'Huntingdale', stops: true },
-    { name: 'Clayton', stops: true },
-    { name: 'Westall', stops: true }
-  ]
+  stops: westall
 }])
