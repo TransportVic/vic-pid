65dc96d5487272027aeab51c2afdfc6e8eb994d1
Eyeball Edward <edwardyeung39@gmail.com>
2025-11-13 19:37:46 +1100

    populate data

--- static/scripts/metro-lcd/service-list.mjs
+++ static/scripts/metro-lcd/service-list.mjs
@@ -3,8 +3,12 @@ import PID from '../pid.mjs'
 export class CompactServiceList extends PID {
 
   #HTML_DATA = (() => {
-    const outerHTML=  $('.service-list.template').outerHTML
-    const rowOuterHTML = $('.service-list.template .service-row.template').outerHTML
+    const template = $('.service-list.compact.template')
+    if (!template) return null
+
+    const outerHTML=  template.outerHTML
+    const rowOuterHTML = $('.service-row.template', template).outerHTML
+
     return {
       outerHTML: outerHTML.replace(rowOuterHTML, '{2}').replaceAll('template', ''),
       rowOuterHTML
@@ -68,4 +72,73 @@ export class CompactServiceList extends PID {
 
 }
 
+export class CompactMultiServiceList extends PID {
+
+  #HTML_DATA = (() => {
+    const template = $('.service-list.multi.template')
+    if (!template) return null
+
+    const outerHTML=  template.outerHTML
+    const rowOuterHTML = $('.service-row.template', template).outerHTML
+
+    return {
+      outerHTML: outerHTML.replace(rowOuterHTML, '{2}').replaceAll('template', ''),
+      rowOuterHTML
+    }
+  })()
+
+  #name
+  description
+  #serviceCount
+
+  #html
+  #mount
+
+  constructor(name, description, serviceCount) {
+    super()
+    this.#name = name
+    this.description = description
+    this.#serviceCount = serviceCount
+
+    this.#html = this.#HTML_DATA.outerHTML
+      .replace('{0}', name)
+      .replace('{1}', description)
+      .replace('{2}', Array(serviceCount).fill(this.#HTML_DATA.rowOuterHTML).join(''))
+  }
+
+  updateServices(services) {
+    let screenServices = services
+      .concat(Array(this.#serviceCount).fill(null))
+      .slice(0, this.#serviceCount)
+
+    this.#updateServices(screenServices)
+  }
+
+  #updateServices(services) {
+    services.forEach((service, i) => {
+      let correspondingRow = $(`.services .service-row:nth-child(${i + 1})`, this.#mount)
+      if (service) {
+        $('.service-platform', correspondingRow).className = `service-platform ${service.line}`
+        $('.service-platform', correspondingRow).textContent = service.platform
+        $('.service-est-time', correspondingRow).textContent = this.formatEstimatedTime(service.estTime)
+      } else {
+        $('.service-platform', correspondingRow).className = 'service-platform no-line'
+        $('.service-platform', correspondingRow).textContent = ''
+        $('.service-est-time', correspondingRow).textContent = ''
+      }
+    })
+  }
+
+  toHTML() {
+    return this.#html
+  }
+
+  mount(query) {
+    this.#mount = $(query)
+    this.#mount.innerHTML = this.toHTML()
+  }
+
+}
+
+window.CompactMultiServiceList = CompactMultiServiceList
 window.CompactServiceList = CompactServiceList
