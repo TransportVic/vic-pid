5aec3bef663536c744f35b1c1d19dd7f4f2c53db
Eyeball Edward <edwardyeung39@gmail.com>
2025-11-13 18:48:46 +1100

    populate service data

--- static/scripts/metro-lcd/service-list.mjs
+++ static/scripts/metro-lcd/service-list.mjs
@@ -1,4 +1,6 @@
-export class CompactServiceList {
+import PID from '../pid.mjs'
+
+export class CompactServiceList extends PID {
 
   #HTML_DATA = (() => {
     const outerHTML=  $('.service-list.template').outerHTML
@@ -14,8 +16,10 @@ export class CompactServiceList {
   #serviceCount
 
   #html
+  #mount
 
   constructor(name, lineClass, serviceCount) {
+    super()
     this.#name = name
     this.#lineClass = lineClass
     this.#serviceCount = serviceCount
@@ -26,8 +30,40 @@ export class CompactServiceList {
       .replace('{2}', Array(serviceCount).fill(this.#HTML_DATA.rowOuterHTML).join(''))
   }
 
+  updateServices(services) {
+    let screenServices = services
+      .concat(Array(this.#serviceCount).fill(null))
+      .slice(0, this.#serviceCount)
+
+    this.#updateServices(screenServices)
+  }
+
+  #updateServices(services) {
+    services.forEach((service, i) => {
+      let correspondingRow = $(`.services .service-row:nth-child(${i + 1})`, this.#mount)
+      if (service) {
+        // correspondingRow.className = `service-row ${service.disruptions.length > 0 ? 'disrupted' : ''}`
+        $('.service-sch-time', correspondingRow).textContent = service.schTime
+        $('.service-destination', correspondingRow).textContent = service.destination
+        $('.service-platform', correspondingRow).textContent = service.platform
+        $('.service-est-time', correspondingRow).textContent = this.formatEstimatedTime(service.estTime)
+      } else {
+        correspondingRow.className = `service-row`
+        $('.service-sch-time', correspondingRow).textContent = '--'
+        $('.service-destination', correspondingRow).textContent = '--'
+        $('.service-platform', correspondingRow).textContent = ''
+        $('.service-est-time', correspondingRow).textContent = ``
+      }
+    })
+  }
+
   toHTML() {
     return this.#html
+  }
+
+  mount(query) {
+    this.#mount = $(query)
+    this.#mount.innerHTML = this.toHTML()
   }
 
 }
